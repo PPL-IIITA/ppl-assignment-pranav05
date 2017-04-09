0767c9be2abf1d2cc54ee5be03c16ef1bbaabd15
pranav05 <pranavmaheshwary005@gmail.com>
2017-04-09 22:41:28 +0530

    Add files via upload

---
+++ Q2/Girl.java
@@ -0,0 +1,14 @@
+/**Class of Girls with Attributes*/
+public class Girl {
+    String name;        /**Name of Girl*/  
+    int att;            /**Attractiveness*/
+    double main_bud;    /**Maintainence Budget*/
+    int iq;             /**Intelligence Level*/
+    String bf;         /**Boyfriend Name*/
+    int bf_id;
+    String stat;        /**Status*/
+    int gift=0;
+    int happy=0;
+    int compatible=0;
+    int comp_stat=0;
+}
